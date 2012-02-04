054ca76b247ca56896064aa0b121ee49441cc326
finalart2005 <finalart2005@9302ae2c-101b-241b-75b9-6ca8f23f1479>
2012-02-04 23:35:03 +0000

    converted to maven

--- JavaPong/src/view/Window.java
+++ JavaPong/src/view/Window.java
@@ -1,5 +1,12 @@
 package view;
 
-public class Window {
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+	private Game game;
+	private PaintPanel paintPanel;
 	
+	public Window(int width, int height, Game game){
+		this.game = game;
+	}
 }
