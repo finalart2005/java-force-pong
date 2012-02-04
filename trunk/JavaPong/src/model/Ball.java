437a69a1d5294d06f1f227078a43850bd61f2f27
finalart2005 <finalart2005@9302ae2c-101b-241b-75b9-6ca8f23f1479>
2012-02-04 22:55:22 +0000

    opzet

--- trunk/JavaPong/src/model/Ball.java
+++ trunk/JavaPong/src/model/Ball.java
@@ -2,4 +2,9 @@ package model;
 
 public class Ball extends Item {
 	
+	public Ball(PongImage image) {
+		super(image);
+		// TODO Auto-generated constructor stub
+	}
+	
 }
--- trunk/JavaPong/src/model/Item.java
+++ trunk/JavaPong/src/model/Item.java
@@ -1,8 +1,14 @@
 package model;
 
 public class Item {
-	private PongImage image;
-	private double xPos, yPos;
+	private PongImage	image;
+	private double		xPos, yPos;
 	
-	public Item(PongImage image);
+	public Item(PongImage image) {
+		this.image = image;
+	}
+	
+	public PongImage getImage() {
+		return image;
+	}
 }
