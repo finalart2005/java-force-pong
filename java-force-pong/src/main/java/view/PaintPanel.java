058d9772c0301761e0fa4c77ed932092c26b5347
finalart2005 <finalart2005@9302ae2c-101b-241b-75b9-6ca8f23f1479>
2012-02-05 05:08:19 +0000

    Testing some graphics

--- java-force-pong/src/main/java/view/PaintPanel.java
+++ java-force-pong/src/main/java/view/PaintPanel.java
@@ -1,7 +1,63 @@
 package view;
 
-import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
 
-public class PaintPanel extends JFrame {
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import listeners.PaintPanelListener;
+
+public class PaintPanel extends JPanel {
+	public Window				window;
+	
+	private PaintPanelListener	listener;
+	private Timer				timer;
+	
+	public double				phase;
+	
+	public PaintPanel(Window window) {
+		this.window = window;
+		
+		buildListener();
+		timer = new Timer(20, listener);
+		timer.start();
+		
+		setBackground(new Color(0, 0, 32));
+		
+		setDoubleBuffered(true);
+	}
+	
+	public void buildListener() {
+		listener = new PaintPanelListener(this);
+		addMouseListener(listener);
+		addMouseMotionListener(listener);
+		addMouseWheelListener(listener);
+		addComponentListener(listener);
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		// draw background
+		g.setColor(getBackground());
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		Random random = new Random();
+		
+		for (int i = 0; i < 1000; i++) {
+			int x = random.nextInt(getWidth());
+			int y = random.nextInt(getHeight());
+			int width = random.nextInt(30) + 20;
+			int height = random.nextInt(30) + 20;
+			g.setColor(new Color(random.nextInt(1 << 23)));
+			g.fillOval(x, y, width, height);
+		}
+		
+		g.setColor(Color.RED);
+		int width = (int) (Math.cos(Math.PI * 2 * phase) * 60 + 100);
+		int height = (int) (Math.sin(Math.PI * 2 * phase) * 60 + 100);
+		g.fillOval(listener.mouseLocation.x - width / 2, listener.mouseLocation.y - height / 2, width, height);
+	}
 	
 }
--- java-force-pong/src/main/java/view/Window.java
+++ java-force-pong/src/main/java/view/Window.java
@@ -1,14 +1,69 @@
 package view;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+import listeners.MenuListener;
 import model.Game;
 
-public class Window extends JFrame {
+public class Window {
 	private Game		game;
+	public JFrame		frame;
 	private PaintPanel	paintPanel;
 	
-	public Window(int width, int height, Game game) {
+	public Window(final int width, final int height, Game game) {
 		this.game = game;
+		
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI(width, height);
+			}
+		});
+	}
+	
+	private void createAndShowGUI(int width, int height) {
+		// Create and set up the window.
+		frame = new JFrame("Java Force Pong");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Set up the content pane, where the "main GUI" lives.
+		Container contentPane = frame.getContentPane();
+		JPanel frameContents = new JPanel();
+		frameContents.setLayout(new BorderLayout(5, 5));
+		
+		// Start creating and adding components.
+		paintPanel = new PaintPanel(this);
+		paintPanel.setPreferredSize(new Dimension(width, height));
+		
+		// Adding components to JPanel, which is then added to the contentPane
+		frameContents.add(BorderLayout.CENTER, paintPanel);
+		contentPane.add(frameContents);
+		contentPane.validate();
+		
+		// Set up the menu bar, which appears above the content pane.
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menu = new JMenu("File");
+		
+		JMenuItem menuItem = new JMenuItem("Exit");
+		menuItem.addActionListener(new MenuListener(this));
+		menu.add(menuItem);
+		
+		menuBar.add(menu);
+		frame.setJMenuBar(menuBar);
+		
+		// Show the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public void repaint() {
+		frame.repaint();
 	}
 }
