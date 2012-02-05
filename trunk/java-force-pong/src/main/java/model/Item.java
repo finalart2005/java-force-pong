45fba26156884d4bc72208dc623ae94154a4dd29
timvm91 <timvm91@9302ae2c-101b-241b-75b9-6ca8f23f1479>
2012-02-05 02:19:50 +0000

    Begin posities game, ball, item

--- trunk/java-force-pong/src/main/java/model/Item.java
+++ trunk/java-force-pong/src/main/java/model/Item.java
@@ -3,6 +3,8 @@ package model;
 public class Item {
 	private PongImage	image;
 	private double		xPos, yPos;
+	private double		xVel, yVel;
+	private double		timestep = 0.5;
 	
 	public Item(PongImage image) {
 		this.image = image;
@@ -10,5 +12,26 @@ public class Item {
 	
 	public PongImage getImage() {
 		return image;
+	}
+	
+	public void tick() {
+		 setxPos(getxPos() + timestep * xVel);
+		 setyPos(getyPos() + timestep * yVel);
+	}
+
+	public void setxPos(double xPos) {
+		this.xPos = xPos;
+	}
+
+	public void setyPos(double yPos) {
+		this.yPos = yPos;
+	}
+
+	public double getxPos() {
+		return xPos;
+	}
+	
+	public double getyPos() {
+		return yPos;
 	}
 }
