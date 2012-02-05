2d2b28c7cb4e17242107bb34e6b1662f95732b44
timvm91 <timvm91@9302ae2c-101b-241b-75b9-6ca8f23f1479>
2012-02-05 02:19:50 +0000

    Begin posities game, ball, item

--- java-force-pong/src/main/java/model/Ball.java
+++ java-force-pong/src/main/java/model/Ball.java
@@ -5,6 +5,10 @@ public class Ball extends Item {
 	public Ball(PongImage image) {
 		super(image);
 		// TODO Auto-generated constructor stub
+		
 	}
 	
+	public double getBallCoordinates(){
+		return 0;
+	}
 }
--- java-force-pong/src/main/java/model/Game.java
+++ java-force-pong/src/main/java/model/Game.java
@@ -1,11 +1,31 @@
 package model;
 
+import java.util.*;
+
+
 public class Game {
+	
+	int numPlayers = getNumPlayers();
+	List<Player> playerList = new ArrayList<Player>(numPlayers);
 	
 	public Game() {
 		
 	}
 	
-	private void tick() {
+	public void tick() 
+	{
+		 for (Item item : itemSet) 
+		 {
+		   item.tick();
+		 }
 	}
+	
+	public int getNumPlayers()
+	{
+		return numPlayers;
+	}
+	
+	public void addPlayer(Player player) {
+		 playerList.add(player);
+		}
 }
