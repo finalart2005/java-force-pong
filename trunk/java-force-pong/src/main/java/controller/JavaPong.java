795f05fb95710ead7f5ef1f5764bb612858f7a30
finalart2005 <finalart2005@9302ae2c-101b-241b-75b9-6ca8f23f1479>
2012-02-05 05:08:19 +0000

    Testing some graphics

--- trunk/java-force-pong/src/main/java/controller/JavaPong.java
+++ trunk/java-force-pong/src/main/java/controller/JavaPong.java
@@ -1,22 +1,25 @@
 package controller;
 
+import model.Game;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import view.PaintPanel;
-
+import view.Window;
 
 public class JavaPong {
-	private static final Logger	log			= LoggerFactory.getLogger(JavaPong.class);
+	private static final Logger	log	= LoggerFactory.getLogger(JavaPong.class);
+	
+	private final Game			game;
 	
 	private JavaPong() {
-		
+		game = new Game();
+		new Window(512, 512, game);
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("Rick is still a fucking idiot!");
-	
+		
 		new JavaPong();
 	}
 }
---
+++ trunk/java-force-pong/src/main/java/listeners/PaintPanelListener.java
@@ -0,0 +1,94 @@
+package listeners;
+
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+
+import javax.swing.event.MouseInputAdapter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import view.PaintPanel;
+import view.Window;
+
+public class PaintPanelListener extends MouseInputAdapter implements ActionListener, ComponentListener {
+	private static final Logger	log				= LoggerFactory.getLogger(PaintPanelListener.class);
+	
+	private PaintPanel			paintPanel;
+	private Window				window;
+	
+	public Point				mouseLocation	= new Point();
+	
+	public boolean				leftButton;
+	public boolean				middleButton;
+	public boolean				rightButton;
+	
+	public PaintPanelListener(PaintPanel paintPanel) {
+		this.paintPanel = paintPanel;
+		window = paintPanel.window;
+	}
+	
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		if (paintPanel.contains(e.getPoint())) {
+			mouseLocation = e.getPoint();
+		}
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		if (paintPanel.contains(e.getPoint())) {
+			mouseLocation = e.getPoint();
+		}
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		paintPanel.repaint();
+	}
+	
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+	
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+	
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		leftButton = e.getButton() == MouseEvent.BUTTON1 ? false : leftButton;
+		middleButton = e.getButton() == MouseEvent.BUTTON2 ? false : middleButton;
+		rightButton = e.getButton() == MouseEvent.BUTTON3 ? false : rightButton;
+	}
+	
+	@Override
+	public void mouseWheelMoved(MouseWheelEvent e) {
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		paintPanel.phase = (paintPanel.phase + .05) % 1;
+		paintPanel.repaint();
+	}
+	
+	public void componentHidden(ComponentEvent arg0) {
+	}
+	
+	public void componentMoved(ComponentEvent arg0) {
+	}
+	
+	public void componentResized(ComponentEvent arg0) {
+	}
+	
+	public void componentShown(ComponentEvent arg0) {
+	}
+}
